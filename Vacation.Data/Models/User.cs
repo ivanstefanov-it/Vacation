24cad842c98127924e64d54fc5f10e32ef04e406
teddy <teddye30@gmail.com>
2022-07-06 13:11:32 +0300

    another changes

--- Vacation.Data/Models/User.cs
+++ Vacation.Data/Models/User.cs
@@ -5,6 +5,7 @@
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public string BirthPlace { get; set; }
         //public DateOnly BirthDate { get; set; }
         public string Country { get; set; }
